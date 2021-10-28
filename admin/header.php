2c772603fb98e39c4819587ae53631746a8e034b
phamhongthang <90234115+phamhongthang@users.noreply.github.com>
2021-10-28 21:48:37 +0700

    Add files via upload

---
+++ admin/header.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Quản lý thi trắc nghiệm</title>
+  </head>
+  <body>
+    <h1 class="bg-light">Hệ thống quản lý nhân viên</h1>
+    <div class="col-md-6  ">
+        <ul class="nav justify-content-end ">
+          <li class="nav-item ">
+            <a class="nav-link active text-red" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link text-red" href="index.php">Admin</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-red" href="#">Books</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link  text-red">Contact</a>
+          </li>
+        </ul>
+      </div>
+  </body>
+</html>
