9108f7ee9169dbd76c6bb4d29eefa12136e9f413
Thuy1001 <90234782+Thuy1001@users.noreply.github.com>
2021-11-02 02:21:35 +0700

    Lỗi(đã thêm đc sản phẩm vào giỏ hàng)

--- cart.php
+++ cart.php
@@ -1,86 +1,87 @@
-<?php include 'config.php'; ?>
-<?php include 'header.php'; ?>
+<?php session_start()?>
+<?php include "header.php" ?>
+<style>
+    img{
+        width: 30%;
+    }
+   
+    </style>
 
-
-<nav class="navbar navbar-light bg-img">
-        <div class="container-fluid">
-            <form class="d-flex mx-auto col-md-6">
-                <input class="form-control me-2" type="search" placeholder="Search for Book.." aria-label="Search">
-                <a class="nav-link" href="search.php"><button class ="btn btn-outline-success" type="submit">Search </buton></a>
-                
-            </form>     
-        </div>
-        <!-- end container-fluid -->
-    </nav>
-<div class="product-cart-container">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 clearfix">
-                <h2 class="section-head">Giỏ hàng của tôi</h2>
-                <table class="table">
- 
-                <table class="table">
-            <thead>
-                <tr>
-                <th scope="col">Mã sản phẩm</th>
-                <th scope="col">Tên sản phẩm</th>
-                <th scope="col">Hình ảnh</th>
-                <th scope="col">Giá</th>
-                <th scope="col">Số lượng</th>
-                <th scope="col">Thanh toán </th>
-                <th scope="col">Xóa  </th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                    
-                    $sql= "SELECT*FROM products";
-                    $result = mysqli_query($conn,$sql);
-                    if(mysqli_num_rows($result)>0)
-                    while($row =mysqli_fetch_assoc($result)){
-                        echo'<tr>';
-                        echo'<th scope="row">'.$row ['pro_id'].'</th>';
-                        echo'<td>'.$row['pro_title'].'</td>';
-                        echo'<td>'
-                        ?>
-                        <?php  if($row['feature_image'] != ''){ ?>
-                                <img src="images/<?php echo $row['feature_image']; ?>" alt="<?php echo $row['feature_image']; ?>" width="100px"/>
-                        <?php }else{ ?>
-                            <img src="images/index.png" alt="" width="100px"/><?php
-                       }  
-                       
-                        '</td>';
-                        
-                        echo'<td>'.$row['pro_price'].'</td>';
-                        echo'<td>'.$row['pro_qty'].'</td>';
-                        echo'<td>'.$row['total_amount'].'</td>';
-                        echo'<td><a href="deleteCart.php?id='.$row['pro_id'].'"><i class="fas fa-user-times"></i></a></td>';
-                        echo'</tr>';
-                    
-                    }
-                ?>
-                <tr>
-                <th scope="row">&nbsp</th>
-                <td>Tổng tiền</td>
-                <td>&nbsp</td>
-                <td>&nbsp</td>
-                <td>&nbsp</td>
-                <td>300000</td>
-                </tr>
-               
-                
-            </tbody>
-            </table>
-                    
+<?php include "config.php"?>
+<?php
+    if(isset($_SESSION["cart"])){
+        $_SESSION["cart"]=array();
+    }
+    if(isset($_GET['action'])){
+        switch($_GET['action']){
+            case"add";
+                foreach($_POST['quantity'] as  $pro_id =>$pro_qty ){
+                $_SESSION["cart"][$pro_id]= $pro_qty;
+            }
+    
+           break;
+        }
+    }
+   if(!empty($_SESSION["cart"])){
+    $result=mysqli_query($conn,"SELECT pro_title,feature_image,pro_price FROM `products` WHERE `pro_id` IN (3) ORDER BY `pro_title` ASC");
+    //var_dump($result);exit;
+   }
   
-                        <div class="empty-result">
-                            Your cart is currently empty.
-                        </div>
-                    <?php ?>
-                
+    ?>
 
-            </div>
+       
+    <body>
+   
+    <form pro_id="cart-form"action="cart.php?action=submit "method="post">
+    <table >
+    <tr>
+      <td class="pro_number">STT</td>
+      <td class="pro_title">Tên sản phẩm</td>
+      <td class="featrure_image">Hình ảnh</td>
+      <td class="pro_price">Giá</td>
+      <td class="pro_qty">Số lượng</td>
+      <td class="total_amount">Thành tiền</td>
+      <td class="delete">Xóa</td>
+    </tr>
+  </thead>
+  <tbody>
+     
+      <?php 
+       $num=1;
+      while($row =mysqli_fetch_array($result)){
+        // var_dump($row);exit;?>
+      <?php
+    }
+    ?>
+  <tr>
+      <td class="pro_number"><?= $num++?></td>
+      <td class="pro_title"><?= $row ['pro_title']?></td>
+      <td class="featrure_image"><img src="<?php $row['feature_image']?>"/></td>
+      <td class="pro_price"><?= $row['pro_price']?></td>
+      <td class="pro_qty"><input type="text" value=""name="quantity[<?php=$row['pro_id']?>]"/></td>
+      <td class="total_amount"><?= $row['pro_price']?>Thành tiền</td>
+      <td><a href="deleteEmployee.php?id='.$row['emp_id'].'"><i class="fas fa-user-times"></i></a></td>
+    </tr>
+  
+
+    <tr>
+    <td class="pro_number">&nbsp</td>
+      <td class="pro_title">Tổng thanh toán</td>
+      <td class="featrure_image">&nbsp</td>
+      <td class="pro_price">&nbsp</td>
+      <td class="pro_qty">&nbsp</td>
+      <td class="total_amount"></td>
+      <td><a href="deleteEmployee.php?id='.$row['emp_id'].'"><i class="fas fa-user-times"></i></a></td>
+    </tr>
+  </body>
+</table>
+<div id="form-button">
+    <input type="submit"name="update_click" value='Cập nhật'/>
         </div>
-    </div>
-</div>
+      <div><lable>Người nhận:</label><input type ="text" value="" name="name"/></div>
+      <div><lable>Điện thoại:</label><input type ="text" value="" name="phone"/></div>
+      <div><lable>Địa chỉ:</label><input type ="text" value="" name="address"/></div>
+      <div><lable>Ghi chú:</label><textarea  name="note"cols="$0"rows="?"></textarea></div>
+    <input type="submit"name="order_click" value='Đặt hàng'/>
+    </form>
 <?php include 'footer.php'; ?>
