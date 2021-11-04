70f22281a3c7d60fd0aa8e79b0f16f2c6e51d957
phamhongthang <phamhongthang201@gmail.com>
2021-11-05 03:00:06 +0700

    Hoàn thành

--- footer.php
+++ footer.php
@@ -1,12 +1,30 @@
-<!-- Optional JavaScript; choose one of the two! -->
+<!-- footer -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 social text-center letf">
+                <ul>
+                    <li>
+                    <li><i class="fab fa-facebook"></i><span>: BookStore</span></li>
+                    </li>
+                    <li>
+                    <i class="fa fa-phone" ></i><span>: 0376533018</span>
+                    </li>
+                    <li>
+                    <i class="fa fa-envelope" ></i><span>: phamhongthang201@gmail.com</span>
+                    </li>
+                </ul>
+            </div> <!--end col-->
+        </div> <!--end row-->
+    </div> <!--end container-fluid-->
+</div>
+    <!-- end footer -->
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
+</div>
+    <!-- end wrapper -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="bootstrap/js/bootstrap.js"></script>
+    <script src="bootstrap/bootstrap@5.0.2"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-   
-  </body>
+</body>
 </html>
--- header.php
+++ header.php
@@ -1,3 +1,18 @@
+<?php  session_start(); ?>
+
+<?php 
+      $conn=mysqli_connect('localhost','root','','bookstore');
+      if(!$conn){
+          die("không thể kết nối");
+      }
+
+    $sql   ="SELECT * FROM categories";
+
+    $res   = mysqli_query($conn, $sql);
+
+    $count = mysqli_num_rows($res);
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -6,7 +21,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
+    <title>Restaurant Website</title>
     <!-- css bootstrap -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
     <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
@@ -19,36 +34,67 @@
             <div class="row">
                 <div class="col">
                     <!-- logo -->
-                    <nav class="navbar navbar-expand-lg navbar-light">
+                    <nav class="navbar navbar-expand-lg navbar-light" method="post" action="">
                     <div class="container-fluid">
                         <a class="navbar-brand" href="index.php">BOOKSTORE</a>      
                         <div class="container1">
                             <ul class="nav justify-content-end">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="../index.php"><i class="fas fa-home"></i> Trang chủ</a>
-                                </li>
 
                                 <li class="nav-item">
-                                    <a class="nav-link" href="dashboard.php"><i class="fas fa-tachometer-alt-fastest"></i> Bảng điều khiển</a>
-                                </li>
-  
-                                <li class="nav-item">
-                                    <a class="nav-link" href="products.php"><i class="fas fa-book"></i> Sản phẩm</a>
-                                </li> 
-                                
-                                <li class="nav-item">
-                                    <a class="nav-link" href="adm.php"><i class="fas fa-user-shield"></i> Admin</a>
-                                </li>
-
-                                <li class="nav-item">
-                                    <a class="nav-link" href="user.php"><i class="fas fa-user"></i> Người dùng</a>
-                                </li>
-   
-                                <li class="nav-item">
-                                    <a class="nav-link" href="index.php"><i class="fas fa-sign-out-alt"></i> Thoát</a>
+                                    <a class="nav-link" href="index.php"><i class="fas fa-home"></i> Trang chủ</a>
                                 </li>
 
                                 
+                                <?php  
+                                if(isset($_SESSION['user_name'])){ 
+                                   ?>
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                            Hello, <?php echo$_SESSION['user_name'] ?>
+                                        </a>
+                                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                            <li><a href="../baitaplon/account/logout_user.php" class="dropdown-item nav-link" name='logout'>Đăng xuất</a></li>
+
+                                        </ul> 
+                                </li>
+                               <?php  } else {?> 
+                                <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                            <i class="far fa-user-circle"></i> Tài khoản
+                                        </a>
+                                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                            <li><a href="../baitaplon/account/login.php" class="dropdown-item nav-link">Đăng nhập</a></li>
+                                            <li><a href="../baitaplon/admin/index.php" class="dropdown-item nav-link">Quản trị viên</a></li>
+                                        </ul>
+                                </li>
+                              <?php } ?>
+                        
+                
+                                <li class="nav-item dropdown">
+                                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                <i class="fas fa-book"></i>Thể loại
+                                            </a>
+                                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                         
+                                            <?php
+                                                if($count >0){
+                                                        while ($row=mysqli_fetch_assoc($res)){
+                                                            
+                                                                $cate_id    = $row['cate_id']; 
+                                                                $cate_title = $row['cate_title'];
+                                                                $url        = $row['cate_url'];
+
+                                            ?>
+                                                <li><a href="categories/<?php echo $url ?>?cate_id=<?php echo $cate_id ?>" class="dropdown-item nav-link" href="#"><?php echo $cate_title ?></a></li>
+                                               <?php }} ?>
+                                            </ul>
+                                </li>
+              
+                                
+                                <li class="nav-item">
+                                    <a class="nav-link" href="carts1.php"><i class="fas fa-shopping-cart"></i> Giỏ hàng</a>
+                                </li>
+
                             </ul>
                         </div>
                         <!-- end container -->
